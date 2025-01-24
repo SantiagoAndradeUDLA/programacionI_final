2067e3031ef294d038594121c0a4de9711c032e9
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-01-24 00:32:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 int main() {
     Zona zonas[NUM_ZONAS];
 
-    srand(time(NULL)); // Inicializa la semilla del generador de números aleatorios
+    srand(time(NULL));
 
     if (cargar_datos(zonas, NOMBRE_ARCHIVO) != 0) {
         printf("Error al cargar datos o archivo no existente. Se usarán datos por defecto.\n");
         simular_datos_historicos(zonas);
     }
-    if(ingresar_datos_actuales(zonas)!=0){
+
+    if (ingresar_datos_actuales(zonas) != 0) {
         fprintf(stderr, "Error al ingresar los datos.\n");
         return 1;
     }
+
+    calcular_promedios_historicos(zonas);
+    mostrar_comparacion_oms(zonas);
+
+    calcular_predicciones(zonas);
+    generar_alertas_y_recomendaciones(zonas);
 
     mostrar_predicciones(zonas);
 
